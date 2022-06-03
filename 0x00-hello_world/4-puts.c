e1c7c21a560eb056cbafabec7a4b11cb59782dd4
olaleykhan <realjoy9@gmail.com>
2022-06-02 20:40:31 -0700

    mandotories rest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main- Is the entry point
+ *
+ *   *
+ *
+ *    *
+ *
+ *     *Return 0 to stop the program
+ *
+ *      */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+/**
+ *
+ *  * main - represent Entry Point to the program
+ *
+ *   *
+ *
+ *    * Return: 0 to stop the program
+ *
+ *     */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
