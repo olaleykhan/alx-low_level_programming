a69e9aa64813bf36382d1fbc19c15c1ab26de3bb
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-06 08:40:01 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-/** * main - Prints the alphabet at reverse * * Return: Always (Success) 
+
+/**
+  * main - Prints the alphabet at reverse
+  *
+  * Return: Always (Success)
   */
-int main(void) { char c; for (c = 'z'; c >= 'a'; c--) { putchar(c);
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
 	}
-	putchar('\n'); return (0);
+
+	putchar('\n');
+
+	return (0);
 }
