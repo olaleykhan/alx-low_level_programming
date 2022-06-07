4e4adf299232035d73600112d5d0e36ba4a42948
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-08 00:52:54 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,11 @@
 
 #include "main.h"
 
-
-
 /**
- *
- *  * main - Check description
- *
- *   * Description: It prints the word _putchar, followed by a new line.
- *
- *    * Return: 0.
- *
- *     */
+* main - Check description
+* Description: It prints the word _putchar, followed by a new line.
+* Return: 0.
+*/
 
 int main(void)
 
