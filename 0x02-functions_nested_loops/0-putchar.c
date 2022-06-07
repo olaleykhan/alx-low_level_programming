89300f3a4ca4d66c695575cc9e88888285f5aacc
olaleykhan <realjoy9@gmail.com>
2022-06-07 14:01:51 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Check description
- * Description: It prints the word _putchar, followed by a new line.
+ * Description: It prints the word _putchar followed by a new line.
  * Return: 0.
  */
 int main(void)
