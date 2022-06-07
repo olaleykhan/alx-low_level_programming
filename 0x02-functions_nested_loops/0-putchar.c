8b85b7623e727dd6638045307d2a26ec8f7f8bbc
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-07 21:48:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include "holberton.h"
+#include <unistd.h>
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
  */
 int main(void)
 {
-	char *sh = "Holberton";
+char word[8] = "_putchar";
+int i;
+char new_line = '\n';
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+for (i = 0; i < 8; i++)
+write(1, &word[i], 1);
+write(1, &new_line, 1);
 
-	return (0);
+return (0);
 }
