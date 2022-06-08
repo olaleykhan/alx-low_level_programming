1dc054b2b5b5159361c0bc9d60ae47937f0bed88
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-08 01:30:12 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,15 @@
-Moify 11
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ * @i: print int i
+ * @k: print int k
+ * Return: Always 0.
+ */
+
+int add(int i, int k)
+
+{
+return (i + k);
+}
