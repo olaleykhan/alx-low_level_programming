e73550ab883672745c25da629f12028187f338d6
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-08 01:18:23 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _islower - Short description, single line
- * @c: contains value to be compared
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ * @c: The int to print
  * Return: Always 0.
  */
-int _islower(int c)
+
+int _isalpha(int c)
 
 {
 
-if (c > 'a' && c < 'z')
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 {
 return (1);
 }
