5cfb2dd5710ff9e231e9b7f3ea0ad6d683cbdbed
olaleykhan <realjoy9@gmail.com>
2022-06-07 17:04:40 -0700

    fix 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,41 @@
 #include <unistd.h>
+
 #include "main.h"
 
+
+
 /**
- * print_alphabet - Entry point
- * Return:0
- */
+ *
+ *  * print_alphabet - Entry point
+ *
+ *   * Return:0
+ *
+ *    */
+
+
 
 void print_alphabet(void)
+
 {
-	char c = 'a';
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
+		char c = 'a';
 
-	}
-	_putchar('\n');
+
+
+			while (c <= 'z')
+
+					{
+
+								_putchar(c);
+
+										c++;
+
+
+
+											}
+
+				_putchar('\n');
+
+
 
 }
