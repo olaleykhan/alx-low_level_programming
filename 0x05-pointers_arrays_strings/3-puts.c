88e79713849e92bfbff83dd580f12f8152af2792
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-13 21:05:42 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
-3 3-puts.c
+#include "main.h"
+/**
+ * _puts - pusts to stdout
+ * @str: params
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+
+_putchar('\n');
+}
