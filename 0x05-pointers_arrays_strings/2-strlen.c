c5cb31d4cc760323156d6795dc1264a4c811ab91
Abdullah Olalekan <53246916+olaleykhan@users.noreply.github.com>
2022-06-13 21:04:05 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
-2-file
+#include "main.h"
+/**
+ * _strlen - string lenght
+ * @s: params
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+int count = 0;
+
+while (s[count] != '\0')
+{
+count++;
+}
+
+return (count);
+}
